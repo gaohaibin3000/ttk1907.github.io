d419f25c64fbce3ab0da02a39048e74a65350409
tiankai <88141662@qq.com>
2019-10-17 10:14:19 +0800

    python auto push

---
+++ Java学习笔记/day01/code/demo1_HelloWorld/HelloWorld.java
@@ -0,0 +1,57 @@
+/*
+ * 项目名称 : 编写第一个Java程序
+ * 项目功能 : 打印一句话
+ * 作    者 : 小马哥
+ * 版    本 : v1.0
+ * 所 有 者 : ttk1907
+ * 备    注 : 学习Java从打印Hello,World开始
+ */
+
+public class HelloWorld/*类名*/{
+    /*类体*/
+    public static void main/*主方法,是程序入口*/(String[] args) {/*方法体*/
+    
+    	System.out.println("我就不打印Hello,World!!!");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
